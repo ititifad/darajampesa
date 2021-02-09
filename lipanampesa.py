d59e7a1331c740770ce124326ac673208ae5fe65
ititifad <dex90jay@gmail.com>
2021-02-09 13:16:41 +0300

    added C2b and code refactoring

--- lipanampesa.py
+++ lipanampesa.py
@@ -1,35 +1,16 @@
 import requests
-from datetime import datetime
 import keys
 from requests.auth import HTTPBasicAuth
-import base64
-
-
-unformatted_time = datetime.now()
-formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
-
-data_to_encode = keys.business_shortCode + keys.lipa_na_mpesa_passkey + formatted_time
-encoded = base64.b64encode(data_to_encode.encode())
-#print(encoded) b'MjAyMTAyMDgxOTAzMTg='
-
-decoded_password = encoded.decode('utf-8')
-#print(decoded_password)
-#print(formatted_time, "this is the formatted time")
-
-consumer_key = keys.consumer_key
-consumer_secret = keys.consumer_secret
-api_URL = (
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-)
-r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-
-json_response = r.json()  #{'access_token': 'sd0xlwWY9FlN48M0uaEGroI6Au91', 'expires_in': '3599'}
-
-my_access_token = json_response['access_token']
+from access_token import generate_access_token
+from encode import generate_password
+from utils import get_timestamp
 
 def lipa_na_mpesa():
     
-    access_token = my_access_token
+    formatted_time = get_timestamp()
+    decoded_password = generate_password(formatted_time)
+    
+    access_token = generate_access_token()
     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     headers = { "Authorization": "Bearer %s" % access_token }
     
